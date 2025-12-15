21fe261cd52d5b564d5fcd0cf391257722883db1
subinii97 <subinii97@users.noreply.github.com>
2025-12-15 09:17:23 +0000

    deploy: e20b7847bbd25881343ab77dbac3413adae77671

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,102 +2,102 @@ var store = [{
         "title": "노르웨이 여행 - 오슬로",
         "excerpt":"여행의 시작 여행을 하기로 마음먹은 것은 비트코인 덕분이다. 2017년 말 비트코인과 알트코인들의 급등으로 나는 만족할만한 수익을 얻었다. 더 욕심내기는 싫어서 코인을 모두 팔았다. 나에게는 300만원의 여윳돈이 생겼다. 이 돈으로 갈 만한 나라를 찾아보았다. 의미있는 여행을 하고 싶었고, 오로라를 볼 수 있다는 노르웨이로 결정했다. 나는 노르웨이를 가는 김에 북극도 역시 가보고자...","categories": ["Travel"],
         "tags": ["norway","jongwon"],
-        "url": "/subinii97.github.io/posts/travel/norway-1/",
+        "url": "/posts/travel/norway-1/",
         "teaser": null
       },{
         "title": "노르웨이 여행 - 오슬로 베르겐 기차여행",
         "excerpt":"1월 25일 오늘은 오슬로를 떠나 베르겐으로 가는 날이다. 캐리어를 보내주는 서비스가 있어서 새벽에 내 캐리어를 보냈다. 비용은 한 5만원정도 했던거로 기억한다. 전날 오슬로 중앙역에서 인터넷으로 예약해둔 “Norway in a nutshell Winter Tour” 기차 여행 패키지를 수령해뒀다. 그리고 이 날 다시 오슬로 중앙역에서 기차 여행을 시작했다. 베르겐으로 향하는 열차 기차를 타면서...","categories": ["Travel"],
         "tags": ["norway","jongwon"],
-        "url": "/subinii97.github.io/posts/travel/norway-2/",
+        "url": "/posts/travel/norway-2/",
         "teaser": null
       },{
         "title": "Transition Story 1. 결심",
         "excerpt":"삶을 되돌아보다. 나는 나에게 무지한 사람이었다. 내가 무엇을 좋아하고 무엇에 관심이 있는지 몰랐다. 나는 어릴 때부터 부모님이 하란대로 열심히 살아왔다. 공부 잘하고 착한 아이… 하지만 그게 전부였다. 자아 성찰을 할 만한 여유 없이, 그리고 이유도 알지 못한 채 공부만을 했다. 엄마가 내가 수학을 좋아한다고 하니까 그런줄 알았다. 나는 2022년 8월...","categories": ["Transition"],
         "tags": ["hrt"],
-        "url": "/subinii97.github.io/posts/transition/starting-transition/",
+        "url": "/posts/transition/starting-transition/",
         "teaser": null
       },{
         "title": "Transition Story 2. 변화의 시작",
         "excerpt":"Hormone Replacement Therapy (HRT) 호르몬 대체 요법은 트랜스젠더의 트랜지션의 첫 번째 단계이다. 나는 진단서를 받고 일주일 뒤 HRT 를 위해 병원을 방문하여 혈액검사를 받았다. 그리고 또 일주일 뒤 드디어 첫 여성호르몬 주사를 맞았다. 이렇게 눈 앞이 까매질 정도로, 진땀이 날 정도로 아픈 주사는 처음이었다. 이걸 계속 맞아야한다니 앞날이 막막해졌다. 그와...","categories": ["Transition"],
         "tags": ["hrt"],
-        "url": "/subinii97.github.io/posts/transition/starting-hrt/",
+        "url": "/posts/transition/starting-hrt/",
         "teaser": null
       },{
         "title": "Transition Story 3. 자유",
         "excerpt":"사회복무요원 소집해제 나는 2022년 9월부터 사회복무요원으로 복무했다. 21개월의 복무를 마치고 이등병으로 소집해제 했다. 사실 나와 같은 트랜스젠더인 사람들은 군대 가는 것을 꺼린다. 나도 역시 다른 남자들과 다르다고 생각하며 복무를 했고 불편한 부분도 있었다. 사실 2023년 4월부터 호르몬 치료를 시작했기 때문에 2023년 10월에는 복무를 중단할 수 있었다. 하지만 나는 그 당시에는...","categories": ["Transition"],
         "tags": ["military"],
-        "url": "/subinii97.github.io/posts/transition/freedom/",
+        "url": "/posts/transition/freedom/",
         "teaser": null
       },{
         "title": "Transition Story 4. 커밍아웃의 계기",
         "excerpt":"Triggered by the Event 2025년 1월 18일 강동성심병원에서는 성별확정수술 세미나와 KITE (Korean Initiative for Transgender Health) 의 \b트랜스젠더 코호트 연구 조사 발표가 있었다. 나는 성별확정수술에 관심이 있었고 호르몬 치료하는 병원에서 KITE 연구를 진행해서 참여하고 있었다. 막상 결과는 알 것 같으면서도 다른 사람들은 어떻게 정체화하고 자신의 신체를 어떻게 변화시키고 있는지 궁금했다....","categories": ["Transition"],
         "tags": ["comingout"],
-        "url": "/subinii97.github.io/posts/transition/coming-out-of-the-closet/",
+        "url": "/posts/transition/coming-out-of-the-closet/",
         "teaser": null
       },{
         "title": "Transition Story 5. 부모님께 커밍아웃",
         "excerpt":"1월 18일 이후 1월 19일은 부모님의 결혼기념일이었다. 삼원가든에서 가족과 간만의 근사한 식사를 했다. 불고기와 된장찌개가 맛있었다 식사를 마치고 나는 전날의 여운을 느꼈다. 내가 온전히 받아들여지고 내가 생각하는 것을 거짓없이 솔직하게 나눌 수 있었던 시간이다. 내가 행복감을 느낄수록 아이러니하게도 불행해지는 느낌이었다. 부모님께 차마 말할 엄두가 안났다. 그렇게 방에 처박혀서 이불 뒤집어쓰고...","categories": ["Transition"],
         "tags": ["comingout"],
-        "url": "/subinii97.github.io/posts/transition/coming-out-to-parents/",
+        "url": "/posts/transition/coming-out-to-parents/",
         "teaser": null
       },{
         "title": "Transition Story 6. 첫 수술",
         "excerpt":"사실 부모님께 커밍아웃하기 전에도 성형에 대해서는 하고 싶은 부위가 있었다. 중학교 때 얼굴을 맞는 바람에 코가 휘고 매부리가 심했다. 그래서 성형을 한다면 코를 가장 먼저 생각했다. 사실 트랜스젠더에게 있어 성형수술은 필수라고 하는 사람들이 많다. 당연할 수밖에 없는게 성별 불쾌감 (Gender Dysphoria)는 지정 성별의 신체와 나의 성별 정체성과 다를 때 느낄...","categories": ["Transition"],
         "tags": ["ffs"],
-        "url": "/subinii97.github.io/posts/transition/my-first-surgery/",
+        "url": "/posts/transition/my-first-surgery/",
         "teaser": null
       },{
         "title": "Transition Story 7. 조급함",
         "excerpt":"코 수술 이후 코 수술 이후 열심히 회복했다. 많이 걸으면서 붓기를 뺐다. 마침 벚꽃 시즌이어서 엄마랑 같이 많이 걸었다. 많이 걸으면서 내 정체성에 대한 이야기를 솔직하게 했다. 엄마는 서서히 나의 그런 성별 불쾌감을 이해해주고 존중해주었다. 여의도 윤중로에 벚꽃이 한창이다. 2025년 4월 8일 여의도 시내 어느 건물 앞의 벚꽃이다. 2025년 4월...","categories": ["Transition"],
         "tags": ["ffs"],
-        "url": "/subinii97.github.io/posts/transition/my-second-surgery/",
+        "url": "/posts/transition/my-second-surgery/",
         "teaser": null
       },{
         "title": "Transition Story 8. 수술 후 우울",
         "excerpt":"안면 수술이 끝나고 마취에서 깼다. 기관 삽관으로 목이 타들어갔고 마취제로 인해서 구토감이 올라왔다. 토를 수차례 했다. 어느 정도 안정이 되었을 때 간호사 분이 어머니한테 전화왔었다고 알려주셨다. 나는 그 순간 뭔가 머리를 세게 얻어맞은 느낌이었다. 얼굴 성형을 결정할 때 나는 조급함에 엄마에게는 통보했고 아빠에게는 말하지 않았다. 나중에 아빠는 엄마에게 들어서 알게...","categories": ["Transition"],
         "tags": ["ffs"],
-        "url": "/subinii97.github.io/posts/transition/depression/",
+        "url": "/posts/transition/depression/",
         "teaser": null
       },{
         "title": "Accident 1. 마른 하늘의 날벼락",
         "excerpt":"전날까지는 아주 좋았다. 나름 신체적으로도 정신적으로도 회복하는 중이었다. 나는 저번 수술의 경과를 볼 겸 실밥도 풀 겸 병원에 엄마와 함께 가기로 했다. 병원에 가기 전에 점심을 먹고 가면 딱이라고 생각했다. 나는 평양냉면 맛집을 찾아다니고 있었다. 논현동의 평양냉면집을 가보고 싶었다. 차를 끌고 평양냉면집 주차장으로 들어갔다. 뭔지 모르게 어수선한 분위기는 있었지만 여느...","categories": ["Diary"],
         "tags": ["caraccident"],
-        "url": "/subinii97.github.io/posts/diary/car-accident/",
+        "url": "/posts/diary/car-accident/",
         "teaser": null
       },{
         "title": "Accident 2. 교통사고 이후 또 다시 무너짐",
         "excerpt":"6월 12일 교통사고가 났다. 나는 피해자로서 그리고 보호자로서 해야 할 일을 해야했다. 우선 당일에는 보험처리를 해야했다. 주차되어 있던 내 차도 파손돼서 보험 회사에 대물 접수를 했다. 그리고 엄마와 나의 부상에 대한 상대측 보험사에 대인 접수를 했다. 상대측 보험사에서는 계속해서 환자의 상태를 궁금해했지만 자세하게 말하기 싫었다. 강남경찰서에서도 전화가 왔다. 엄마랑 내가...","categories": ["Diary"],
         "tags": ["caraccident"],
-        "url": "/subinii97.github.io/posts/diary/hospital-life-1/",
+        "url": "/posts/diary/hospital-life-1/",
         "teaser": null
       },{
         "title": "Transition Story 9. 성별확정수술",
         "excerpt":"성별확정수술(Gender Reassignment Surgery, Gender Affirming Surgery, Gender Confirmation Surgery)은 이전에는 성전환 수술 (Sex Reassignment Surgery)라고 불리던 성별 확정 치료 중 하나이다. 사실 사람마다 느끼는 성별 불쾌감 (Gender Dysphoria)가 다르기 때문에 요구되는 성별확정수술의 종류는 사람마다 다르다. 대표적인 성별확정수술은 생식기 재건 수술이고 원하는 성별의 생식기 외형을 만들게 된다. 여성화 수술에는 외음부 성형술...","categories": ["Transition"],
         "tags": ["bottomsurgery"],
-        "url": "/subinii97.github.io/posts/transition/grs-consultation/",
+        "url": "/posts/transition/grs-consultation/",
         "teaser": null
       },{
         "title": "Accident 3. 양평에서 여름나기",
         "excerpt":"양평에 있는 재활병원에 와서는 보호자로서 해야할 일이 너무 많았다. 입원 수속부터 입원 후 검사, 재활 치료 그리고 그 밖의 모든 간병일들까지 꽤나 체력이 필요한 일이었다. 엄마가 스스로 거동이 안되기 때문에 휠체어로 이동을 해야해서 내가 항상 붙어있었다. 재활 시간표도 오전 오후로 빽빽하게 짜여있어서 바쁘게 움직였다. 엄마가 가는 모든 곳이라면 따라다녔다. 화장실,...","categories": ["Diary"],
         "tags": ["caraccident"],
-        "url": "/subinii97.github.io/posts/diary/hospital-life-2/",
+        "url": "/posts/diary/hospital-life-2/",
         "teaser": null
       },{
         "title": "Hello World",
         "excerpt":"Hello World   블로그는 처음이라 어색하다…   어떤 글을 쓸지는 모르겠지만   그냥 내가 사는 이야기를 기록으로 남기려고 한다.   블로그의 주제? 는 딱히 없어서 다양한 주제의 이야기를 하고 싶다.   주로 내가 경험하는 것, 나의 정체성에 관한 것 등…   세상에게 나를 소개하는 느낌으로 써보려고 한다.   세상아 안녕! Hello World!   ","categories": ["Diary"],
         "tags": ["hi"],
-        "url": "/subinii97.github.io/posts/diary/Hello-World/",
+        "url": "/posts/diary/Hello-World/",
         "teaser": null
       },{
         "title": "Accident Final. 퇴원",
         "excerpt":"드디어 퇴원이다. 2025년 6월 12일에 사고나서 입원한지 딱 6개월만에 엄마는 집에서 잘 수 있게 되었다. 병원 생활에서 가장 힘든 점이라고 하면 다인실에서 다른 사람들에 의한 스트레스였다. 원치 않는 소음과 조명이 힘들었다. 이제는 그런 것에서 벗어나서 조용하고 어두운 곳에서 잠잘 수 있을 것 같다. 고마운 사람들이 많다. 재활을 담당했던 교수님, 재활치료사들이...","categories": ["Diary"],
         "tags": ["caraccident"],
-        "url": "/subinii97.github.io/posts/diary/hospital-life-fin/",
-        "teaser": "/subinii97.github.io/assets/img/discharge.jpeg"
+        "url": "/posts/diary/hospital-life-fin/",
+        "teaser": "/assets/img/discharge.jpeg"
       },{
         "title": "Transition Story 10. 감사함",
         "excerpt":"‘나’라는 사람? 나는 운이 좋은 것 같다. 운이 나쁜거같지만 운이 좋다. 주변에 많은 좋은 사람들이 있어서 운이 좋다. 과분하다고 느낄정도로 내 주변 사람들이 좋은 사람들이라는 것을 느낀다. 단지 나의 커망아웃을 받아주어서가 아니다. 그 이전에도 내가 해보겠다는거 내 결정들을 지지하고 응원해주던 사람들이다. 나는 그런 응원을 어색해했지만 지금은 진짜로 응원이 된다. 수술을...","categories": ["Transition"],
         "tags": ["bottomsurgery"],
-        "url": "/subinii97.github.io/posts/transition/thank-you/",
+        "url": "/posts/transition/thank-you/",
         "teaser": null
       }]
